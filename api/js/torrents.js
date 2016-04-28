874f7c8edb08318e6a38806c1ff0cba199744dfc
Ryan Beall <beall49@users.noreply.github.com>
2016-04-27 19:10:33 -0700

    Formatting

--- api/js/torrents.js
+++ api/js/torrents.js
@@ -4,54 +4,57 @@ app.controller('TorrentController', function($scope, $interval, $http, $sce, API
     getDownloads();
     $scope.interval = 90000;
     $scope.colorState = "danger";
-    
+
     $interval(function() {
-    	getDownloads();
-    },  
-    $scope.interval);
-        
-    API.getNavBar().success(function(response) {            
-        $scope.NavBar = $sce.trustAsHtml(response);   
+            getDownloads();
+        },
+        $scope.interval);
+
+    API.getNavBar().success(function(response) {
+        $scope.NavBar = $sce.trustAsHtml(response);
     });
 
-    API.getSideBar().success(function(response) {            
-        $scope.SideBar = $sce.trustAsHtml(response);   
-    }); 	
+    API.getSideBar().success(function(response) {
+        $scope.SideBar = $sce.trustAsHtml(response);
+    });
 
-    function getDownloads(){
-        API.getTimeStamp().success(function(response) {	
-        
-            if (response == ''){
-            
-               returnText = `<span>No items are currently downloading</span>`;
-               $scope.Interval = 90000;     
-                      
+    function getDownloads() {
+        API.getTimeStamp().success(function(response) {
+
+            if (response == '') {
+
+                returnText = `<span>No items are currently downloading</span>`;
+                $scope.Interval = 90000;
+
             } else {
-            
-               returnText = `<span> Updates Every 2 Minutes, Last Updated: ${response["ts"]} </span>`;
-               $scope.Interval = 45000;
-               
-	       API.getProgress().success(function(response) {
-	       	   $scope.Bars = response;	
-	       });                           
+
+                returnText = `<span> Updates Every 2 Minutes, Last Updated: ${response["ts"]} </span>`;
+                $scope.Interval = 45000;
+
+                API.getProgress().success(function(response) {
+                    $scope.Bars = response;
+                });
             }
-            $scope.Status = $sce.trustAsHtml(returnText);            
-        });	
-    }    
-    
+            $scope.Status = $sce.trustAsHtml(returnText);
+        });
+    }
+
     $scope.buttonSubmit = function(link_type) {
-      var component = $('input#' + link_type);
-      var magnet = component.val();   	  
-      var data ={"link": magnet, 'link_type' : link_type  };
-      $http.post("./api/php/insert_torrent" , data).success(function(data, status){
-	  component.val("").attr("placeholder", "Link has been added");     
-      });		
-    }  
-        
-    function toggleSideBar(){
-        var screenWidth = $( window ).width();
-        if(screenWidth < 500){
+        var component = $('input#' + link_type);
+        var magnet = component.val();
+        var data = {
+            "link": magnet,
+            'link_type': link_type
+        };
+        $http.post("./api/php/insert_torrent", data).success(function(data, status) {
+            component.val("").attr("placeholder", "Link has been added");
+        });
+    }
+
+    function toggleSideBar() {
+        var screenWidth = $(window).width();
+        if (screenWidth < 500) {
             $("#wrapper").toggleClass("toggled");
-        }                        
-    }	
+        }
+    }
 });
